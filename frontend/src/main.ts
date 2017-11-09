4aa1813e216152c409fb0eebb20d61baa42c7bfa
Cédric Eberhardt <design@frontools.rocks>
2017-11-09 21:06:22 +0100

    split styles and scripts import

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,15 @@
+/* --- Styles --- */
+import "./css-shell/main.css";
+import "./page-styles/base.css";
+import "./page-styles/article.css";
+import "./page-styles/breadcrumbs.css";
+
+/* --- Application --- */
 import Vue from "vue";
 import { LazyMedia } from "./components/lazy-media";
 import { Carousel } from "./components/carousel";
 
 import LoadingPage from "./components/loading-page.vue";
-
-import "./css-shell/main.css";
-import "./page-styles/base.css";
-import "./page-styles/article.css";
-import "./page-styles/breadcrumbs.css";
 
 const vm = new Vue({});
 
