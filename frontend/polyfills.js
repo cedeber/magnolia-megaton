ab76a2a54cfa5340de3badc7565bed99cf473c52
Cédric Eberhardt <design@frontools.rocks>
2017-11-20 17:57:51 +0100

    add other polyfills
    fix #34

---
+++ frontend/polyfills.js
@@ -0,0 +1,13 @@
+try {
+    new MouseEvent('test');
+} catch (e) {
+    var MouseEvent = function (eventType, params) {
+        params = params || { bubbles: false, cancelable: false };
+        var mouseEvent = document.createEvent('MouseEvent');
+        mouseEvent.initMouseEvent(eventType, params.bubbles, params.cancelable, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+
+        return mouseEvent;
+    }
+    MouseEvent.prototype = Event.prototype;
+    window.MouseEvent = MouseEvent;
+}
