ace9465081002f03564d4e0efc2c09cd61e91adc
Cédric Eberhardt <design@frontools.rocks>
2017-11-22 12:35:52 +0100

    sticky component

---
+++ frontend/src/components/sticky.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="o-sticky">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Vue, Component, Prop } from "vue-property-decorator";
+    import "../helpers/vertical-state";
+
+    @Component
+    class Sticky extends Vue {
+        @Prop({type: Number, default: 0}) public marginTop: number;
+
+        public element: HTMLElement | null = null;
+
+        public mounted() {
+            this.element = this.$slots.default[0].elm as HTMLElement;
+
+            if (this.element) {
+                window.addEventListener("scroll", this.stickToTop);
+                this.stickToTop();
+            }
+        }
+
+        public stickToTop() {
+            if (this.element) {
+                const topProgress = this.$el.verticalState.getBoundedState("topProgress", [this.marginTop, 0]);
+
+                if (topProgress >= 1) {
+                    this.element.style.position = "fixed";
+                    this.element.style.top = `${this.marginTop}px`;
+                }
+                else {
+                    this.element.style.position = "";
+                    this.element.style.top = "";
+                }
+            }
+        }
+    }
+
+    export default Sticky;
+</script>
