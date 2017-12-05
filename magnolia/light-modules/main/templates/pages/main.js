abcddcb3a0234b0404976a3d002ac1a4e765b075
Cédric Eberhardt <design@frontools.rocks>
2017-12-05 23:13:51 +0100

    code styling

--- magnolia/light-modules/main/templates/pages/main.js
+++ magnolia/light-modules/main/templates/pages/main.js
@@ -1,6 +1,7 @@
+"use strict";
 var Main = function() {
-    this.getMin = function(v1, v2) { return Math.min(v1, v2); }
-    this.getMax = function(v1, v2) { return Math.max(v1, v2); }
+    this.getMin = function(v1, v2) { return Math.min(v1, v2); };
+    this.getMax = function(v1, v2) { return Math.max(v1, v2); };
 };
 
 new Main();
