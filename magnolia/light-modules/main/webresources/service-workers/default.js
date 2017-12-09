dadea713fdf1e04f77abf1b2d2313f4e30f3ab95
Cédric Eberhardt <design@frontools.rocks>
2017-12-09 15:40:30 +0100

    update worker

--- magnolia/light-modules/main/webresources/service-workers/default.js
+++ magnolia/light-modules/main/webresources/service-workers/default.js
@@ -1,4 +1,64 @@
 /**
+ *
+ * @param {string} cacheName
+ * @param {Map<string, string>} urlMap
+ * @returns {Promise<true|Error>}
+ */
+function preCache(cacheName, urlMap) {
+    return new Promise( async (resolve, reject) => {
+        try {
+            const cache = await caches.open(cacheName);
+
+            for (const url of urlMap.values()) {
+                const match = await caches.match(url, {cacheName: cacheName});
+
+                if (match == undefined) {
+                    try {
+                        const response = await fetch(url);
+
+                        if (response.ok) {
+                            await cache.put(url, response);
+                        }
+                    }
+                    catch(_error) { /* unused */ }
+                }
+            }
+
+            resolve(true);
+        }
+        catch (error) { reject(error); }
+    });
+}
+
+/**
+ *
+ * @param {FetchEvent} fetchEvent
+ * @param {IterableIterator|Array} fallbacks
+ * @param {string} cacheName
+ * @returns {Promise<Response>|undefined}
+ *
+ * @todo do it with iterator
+ */
+async function fetchOrFallback(fetchEvent, fallbacks, cacheName) {
+    try {
+        const response = await fetch(fetchEvent.request);
+
+        if (!response.ok) { throw new Error(); }
+
+        return response;
+    }
+    catch (_error) {
+        for (const fallback of fallbacks) {
+            console.log(fallback);
+            const match = cacheName ? await caches.match(fallback, { cacheName }) : await caches.match(fallback);
+
+            if (match) { return match; }
+        }
+    }
+}
+
+
+/*
  * ┌────────────────────────┐
  * │                        │
  * │     Fallback Image     │
@@ -10,57 +70,22 @@ const fallbackImagesMap = new Map();
 
 fallbackImagesMap.set("error", "/sw-assets/error-connection-failure.svg");
 
-/**
- * Get a fallback image if offline or request error
- * @param {FetchEvent} fetchEvent
- * @returns {Promise<*>}
+
+/*
+ * ┌─────────────────────┐
+ * │                     │
+ * │     Application     │
+ * │                     │
+ * └─────────────────────┘
  */
-async function fetchImageOrFallback(fetchEvent) {
-    try {
-        const response = await fetch(fetchEvent.request);
+const appCacheName = "app-cache-v1";
+const pageCacheMap = new Map();
 
-        if (!response.ok) { throw new Error(); }
+pageCacheMap.set("contact", "/contact.html");
+pageCacheMap.set("home", "/home.html");
 
-        return response;
-    }
-    catch (_error) {
-        return caches.match(fallbackImagesMap.get("error"), {cacheName: fallbackCacheName});
-    }
-}
 
-/**
- * Pre-cache fallback images
- * @returns {Promise<any>}
- */
-function installFallbackImages() {
-    return new Promise( async (resolve, reject) => {
-        try {
-            const fallbackCache = await caches.open(fallbackCacheName);
-
-            for (const fallbackImage of fallbackImagesMap.values()) {
-                const fallbackMatch = await caches.match(fallbackImage, {cacheName: fallbackCacheName});
-
-                if (fallbackMatch == undefined) {
-                    await fallbackCache.add(fallbackImage);
-                }
-            }
-
-            resolve(true);
-        }
-        catch (error) { reject(error); }
-    });
-}
-
-/**
- * ┌───────────────────┐
- * │                   │
- * │     App Cache     │
- * │                   │
- * └───────────────────┘
- */
-const appCache = "app-cache-v1";
-
-/**
+/*
  * ┌────────────────────────┐
  * │                        │
  * │     SERVICE WORKER     │
@@ -70,28 +95,31 @@ const appCache = "app-cache-v1";
 // Pre-cache
 self.addEventListener("install", event => {
     event.waitUntil(Promise.all([
-        installFallbackImages(),
+        preCache(fallbackCacheName, fallbackImagesMap),
+        preCache(appCacheName, pageCacheMap),
     ]));
 });
 
 // Clear all unused caches
 self.addEventListener("activate", event => {
-    event.waitUntil(async function() {
-        const cacheNames = await caches.keys();
-
-        return Promise.all(
-            cacheNames
-                .filter(cacheName => cacheName !== fallbackCacheName && cacheName !== appCache)
-                .map(cacheName => caches.delete(cacheName))
-        );
-    }());
+    event.waitUntil(caches.keys().then(
+        cacheNames => cacheNames
+            .filter(cacheName => cacheName !== fallbackCacheName && cacheName !== appCacheName)
+            .map(cacheName => caches.delete(cacheName))
+    ));
 });
 
 self.addEventListener("fetch", event => {
     const acceptHeader = event.request.headers.get("accept");
+    const url = new URL(event.request.url);
 
+    // images
     if (acceptHeader.indexOf("image/*") >= 0) {
-        event.respondWith(fetchImageOrFallback(event));
+        event.respondWith(fetchOrFallback(event, [fallbackImagesMap.get("error")], fallbackCacheName));
+    }
+    // web pages
+    else if (acceptHeader.indexOf("text/html") >= 0) {
+        event.respondWith(fetchOrFallback(event, pageCacheMap.values(), appCacheName));
     }
 });
 
