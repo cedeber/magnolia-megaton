231e3134dc07791884142d817a1602be7a13b279
Cédric Eberhardt <design@frontools.rocks>
2017-12-09 13:34:01 +0100

    organize code

--- magnolia/light-modules/main/webresources/service-workers/default.js
+++ magnolia/light-modules/main/webresources/service-workers/default.js
@@ -1,6 +1,20 @@
-const currentCache = "cache-v1";
-const fallbackImageUrl = "/sw-assets/error-connection-failure.svg";
+/**
+ * ┌────────────────────────┐
+ * │                        │
+ * │     Fallback Image     │
+ * │                        │
+ * └────────────────────────┘
+ */
+const fallbackCacheName = "fallback-cache-v1";
+const fallbackImagesMap = new Map();
 
+fallbackImagesMap.set("error", "/sw-assets/error-connection-failure.svg");
+
+/**
+ * Get a fallback image if offline or request error
+ * @param {FetchEvent} fetchEvent
+ * @returns {Promise<*>}
+ */
 async function fetchImageOrFallback(fetchEvent) {
     try {
         const response = await fetch(fetchEvent.request);
@@ -10,27 +24,64 @@ async function fetchImageOrFallback(fetchEvent) {
         return response;
     }
     catch (_error) {
-        return caches.match(fallbackImageUrl, {cacheName: currentCache});
+        return caches.match(fallbackImagesMap.get("error"), {cacheName: fallbackCacheName});
     }
 }
 
+/**
+ * Pre-cache fallback images
+ * @returns {Promise<any>}
+ */
+function installFallbackImages() {
+    return new Promise( async (resolve, reject) => {
+        try {
+            const fallbackCache = await caches.open(fallbackCacheName);
+
+            for (const fallbackImage of fallbackImagesMap.values()) {
+                const fallbackMatch = await caches.match(fallbackImage, {cacheName: fallbackCacheName});
+
+                if (fallbackMatch == undefined) {
+                    await fallbackCache.add(fallbackImage);
+                }
+            }
+
+            resolve(true);
+        }
+        catch (error) { reject(error); }
+    });
+}
+
+/**
+ * ┌───────────────────┐
+ * │                   │
+ * │     App Cache     │
+ * │                   │
+ * └───────────────────┘
+ */
+const appCache = "app-cache-v1";
+
+/**
+ * ┌────────────────────────┐
+ * │                        │
+ * │     SERVICE WORKER     │
+ * │                        │
+ * └────────────────────────┘
+ */
+// Pre-cache
 self.addEventListener("install", event => {
-    event.waitUntil(async function() {
-        const cache = await caches.open(currentCache);
-        await cache.addAll([
-            // all files to cache
-            fallbackImageUrl,
-        ]);
-    }());
+    event.waitUntil(Promise.all([
+        installFallbackImages(),
+    ]));
 });
 
+// Clear all unused caches
 self.addEventListener("activate", event => {
     event.waitUntil(async function() {
         const cacheNames = await caches.keys();
 
-        Promise.all(
+        return Promise.all(
             cacheNames
-                .filter(cacheName => cacheName !== currentCache)
+                .filter(cacheName => cacheName !== fallbackCacheName && cacheName !== appCache)
                 .map(cacheName => caches.delete(cacheName))
         );
     }());
@@ -38,8 +89,6 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
     const acceptHeader = event.request.headers.get("accept");
-
-    console.log(event.request.url, acceptHeader);
 
     if (acceptHeader.indexOf("image/*") >= 0) {
         event.respondWith(fetchImageOrFallback(event));
