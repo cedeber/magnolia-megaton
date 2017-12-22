14b40433b8064ba8567f34004e2f8ad798c0b7e1
Cédric Eberhardt <design@frontools.rocks>
2017-12-22 16:41:34 +0100

    simplify taggr

--- frontend/src/devtools/taggr.ts
+++ frontend/src/devtools/taggr.ts
@@ -1,23 +1,18 @@
 declare var process: any;
 
-function getStyle(lvl: number, tag?: boolean) {
-    const bgColor = [undefined, undefined, "#99cc99", "#ffcc66", "#f2777a"];
+function getStyle(lvl: number) {
+    const bgColor = ["#2d2d2d", "#6699cc", "#99cc99", "#ffcc66", "#f2777a"];
+    const txtColor = ["#ffcc66", "#f2f0ec", "#2d2d2d", "#2d2d2d", "#f2f0ec"];
 
-    return tag ?
-        lvl > 0 ? `background: ${bgColor[lvl] || "#393939"}; color: ${bgColor[lvl] ? "#2d2d2d" : "#f2f0ec"}; padding: 2px 0.9em 2px 0.7em; border-top-right-radius: 1.5em; border-bottom-right-radius: 1.5em; font-weight: normal; margin-right: 0.5em;`
-            : `background: ${bgColor[lvl] || "#393939"}; color: ${bgColor[lvl] ? "#2d2d2d" : "#ffcc66"}; padding: 2px 0.7em 2px 0.9em; border-top-left-radius: 1.5em; border-bottom-left-radius: 1.5em; font-weight: bold; margin-right: 1px;`
-        : `background: ${bgColor[lvl] || "#393939"}; color: ${bgColor[lvl] ? "#2d2d2d" : "#ffcc66"}; padding: 2px 0.8em; border-radius: 1.5em; font-weight: bold; margin-right: 0.5em;`;
+    return lvl > 0 ? `background: ${bgColor[lvl] || bgColor[0]}; color: ${txtColor[lvl] || txtColor[0]}; padding: 2px 0.9em 2px 0.7em; border-top-right-radius: 1.5em; border-bottom-right-radius: 1.5em; font-weight: normal; margin-right: 0.5em;`
+        : `background: ${bgColor[lvl] || bgColor[0]}; color: ${txtColor[lvl] || txtColor[0]}; padding: 2px 0.7em 2px 0.9em; border-top-left-radius: 1.5em; border-bottom-left-radius: 1.5em; font-weight: bold; margin-right: 1px;`;
 }
 
 function paint(level: number, tag: string | undefined, message: string | undefined, ...args: any[]) {
-    const defaultLogArgs = tag == undefined ? [
-        `%cfrontools%c${message || ""}`,
+    const defaultLogArgs = [
+        `%cfrontools%c${tag || "…"}%c${message || ""}`,
+        getStyle(0),
         getStyle(level),
-        "color: #2d2d2d; font-weight: bold",
-    ] : [
-        `%cfrontools%c${tag}%c${message || ""}`,
-        getStyle(0, true),
-        getStyle(level, true),
         "color: #2d2d2d; font-weight: bold",
     ];
 
@@ -53,10 +48,10 @@ class Tagger {
         return this;
     }
 
-    public info(message?: string) { return this.spray(1,  message); }
-    public success(message?: string) { return this.spray(2,  message); }
-    public warning(message?: string) { return this.spray(3,  message); }
-    public error(message?: string) { return this.spray(4,  message); }
+    public info(message?: string) { return this.spray(1, message); }
+    public success(message?: string) { return this.spray(2, message); }
+    public warning(message?: string) { return this.spray(3, message); }
+    public error(message?: string) { return this.spray(4, message); }
 
     public list(...args: any[]) { this.args.push(...args); return this; }
     public keep(...args: any[]) { this.keptArgs.push(...args); return this; }
