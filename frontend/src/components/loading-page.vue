1001ce67ecadd537050c586164f35ae685bd6cd3
Cédric Eberhardt <design@frontools.rocks>
2018-01-02 19:57:25 +0100

    fix aria

--- frontend/src/components/loading-page.vue
+++ frontend/src/components/loading-page.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="loading" role="presentation" aria-hidden="true" class="o-loading" v-bind:class="{ 'js-loading': isLoading, 'js-hidden': isHidden }">
+    <div id="loading" aria-hidden="true" class="o-loading" v-bind:class="{ 'js-loading': isLoading, 'js-hidden': isHidden }">
         <div class="o-flex-middle loading-overlay" v-bind:class="{ 'js-loading': isLoading }"><img class="image" src="../assets/square.svg" width="10px" height="10" alt=""></div>
         <div class="opaque-overlay"></div>
         <div class="transparent-overlay"></div>
