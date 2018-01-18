bc1c3edbb4b7a3e357bba047a263e4268d7638ac
Cédric Eberhardt <design@frontools.rocks>
2018-01-18 10:38:20 +0100

    deactivate loading as used only for SPA

--- frontend/src/components/loading-page.vue
+++ frontend/src/components/loading-page.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="loading" aria-hidden="true" class="o-loading" :class="{ 'js-loading': isLoading, 'js-hidden': isHidden }">
-        <div class="o-flex-middle loading-overlay" :class="{ 'js-loading': isLoading }"><img class="image" src="../assets/square.svg" width="10px" height="10" alt=""></div>
+        <div class="o-flex-middle loading-overlay" :class="{ 'js-loading': isLoading }">
+            <img class="image" src="../assets/square.svg" width="10px" height="10" alt="">
+        </div>
         <div class="opaque-overlay"></div>
         <div class="transparent-overlay"></div>
     </div>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,10 @@
 /* --- Shell --- */
-import "./shell.css"; // Extracted separatly by webpack, see webpack.config.js. Injected by FreeMarker
+// Extracted separatly by webpack, see webpack.config.js. Injected by FreeMarker
+import "./shell.css";
 
 /* --- Styles --- */
+// In webpack.config.js you can choose to extract it thanks to the `extractCSS` var
+// If extractCSS == false, the CSS is injected into the HTML by style-loader
 import "./styles/webfonts.css";
 import "./styles/base.css";
 import "./styles/article.css";
@@ -9,20 +12,26 @@ import "./styles/breadcrumbs.css";
 
 /* --- Application --- */
 import Vue from "vue";
+import taggr from "./devtools/taggr";
+
+/* --- Components --- */
+// If a Vue (*.vue) component exists, import only it.
+// The related CSS and TS are linked into the Vue component
 import LazyMedia from "./components/lazy-media";
 import Carousel from "./components/carousel";
-import LoadingPage from "./components/loading-page.vue";
-import taggr from "./devtools/taggr";
 import GoogleMap from "./components/google-map.vue";
 
-// Keep it! If you build the app with NODE_ENV = "production", it won't log.
+// Keep it! If you build the app with NODE_ENV == "production", it won't log.
 taggr().info("Your application is running in development mode.");
 
+// Create the vue instance
 const vm = new Vue({});
 
+// Declare all components, on all pages
 Vue.component("lazy-media", LazyMedia);
 Vue.component("multi-carousel", Carousel);
-Vue.component("loading-page", LoadingPage);
 Vue.component("google-map", GoogleMap);
 
+// Connect the Vue intance to the whole <main id="view"> container
+// Avoid to use the standard DOM API as a virtual-dom will handle it
 vm.$mount("#view");
