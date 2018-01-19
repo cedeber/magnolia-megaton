90b5b297fe7b5f04abfedaf11aa7a58ba7fcc602
Cédric Eberhardt <design@frontools.rocks>
2018-01-19 17:27:05 +0100

    swipe jsdoc

--- frontend/src/components/swipe.ts
+++ frontend/src/components/swipe.ts
@@ -1,5 +1,11 @@
 import { Vue, Component } from "vue-property-decorator";
 
+/**
+ * Swipe Element
+ * Don't forget to style it so that it takes the needed space
+ * @example
+ * <swipe @swipeup="doSomething()" class="o-swipe"></swipe>
+ */
 @Component
 class Swipe extends Vue {
     public blockClickEventDistance = 0;
@@ -11,6 +17,11 @@ class Swipe extends Vue {
     };
     public hasCursorDown = false;
 
+    /**
+     * Block the click Event only if the pointer moves more than 30px
+     * Let the pointer click through the swipe layer
+     * @param {MouseEvent} event
+     */
     public blockClick(event: MouseEvent) {
         if (this.blockClickEventDistance > 30) {
             event.preventDefault();
@@ -20,6 +31,10 @@ class Swipe extends Vue {
         }
     }
 
+    /**
+     * Pointer click/touch
+     * @param {MouseEvent | Touch} event
+     */
     public touchStart(event: MouseEvent | Touch) {
         const startEvent = "TouchEvent" in window && event instanceof TouchEvent ? event.changedTouches[0] : event;
 
@@ -31,6 +46,10 @@ class Swipe extends Vue {
         this.hasCursorDown = true;
     }
 
+    /**
+     * Pointer is moving
+     * @param {MouseEvent | Touch} event
+     */
     public touchMove(event: MouseEvent | Touch) {
         if (this.swipe.move) {
             const moveEvent = "TouchEvent" in window && event instanceof TouchEvent ? event.changedTouches[0] : event;
@@ -39,19 +58,26 @@ class Swipe extends Vue {
                 y: moveEvent.clientY - this.swipe.y,
             };
 
+            // Bigger distance moved
             this.blockClickEventDistance = Math.max(
                 Math.abs(detail.x),
                 Math.abs(detail.y),
                 this.blockClickEventDistance,
             );
 
+            // Send how much the pointer moved horizontally and vertically
             this.$emit("swipemove", detail);
         }
     }
 
+    /**
+     * Pointer released
+     * @param {MouseEvent | Touch} event
+     */
     public touchEnd(event: MouseEvent | Touch) {
         this.hasCursorDown = false;
 
+        // not a single click
         if (this.swipe.move) {
             const endEvent = "TouchEvent" in window && event instanceof TouchEvent ? event.changedTouches[0] : event;
             const now = Date.now();
@@ -60,6 +86,10 @@ class Swipe extends Vue {
                 y: endEvent.clientY - this.swipe.y,
             };
 
+            // A horizontal swipe has to:
+            //   - be done in less than 1 second
+            //   - be moved more than 30 pixels horizontally
+            //   - be moved less than 30 pixels vertically
             if (Math.abs(endEvent.clientY - this.swipe.y) < 30 && now - this.swipe.time < 1000) {
                 if (detail.x > 30) {
                     this.$emit("swiperight");
@@ -68,6 +98,10 @@ class Swipe extends Vue {
                 }
             }
 
+            // A vertical swipe has to:
+            //   - be done in less than 1 second
+            //   - be moved more than 30 pixels vertically
+            //   - be moved less than 30 pixels horizontally
             if (Math.abs(endEvent.clientX - this.swipe.x) < 30 && now - this.swipe.time < 1000) {
                 if (detail.y > 30) {
                     this.$emit("swipedown");
@@ -76,11 +110,16 @@ class Swipe extends Vue {
                 }
             }
 
+            // Emit the move event with the distance informations
             this.$emit("swipeend", detail);
             this.swipe.move = false;
         }
     }
 
+    /**
+     * Consider wheel mouse event as swipe up and down
+     * @param {WheelEvent} event
+     */
     public onWheel(event: WheelEvent) {
         if (event.deltaY > 0) {
             this.$emit("swipeup");
