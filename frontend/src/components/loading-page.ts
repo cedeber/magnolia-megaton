fee2aed18195213f66db1c1710ad9633bbb8b4d7
Cédric Eberhardt <design@frontools.rocks>
2018-01-22 08:57:25 +0100

    move applyBeforeQuit

--- frontend/src/components/loading-page.ts
+++ frontend/src/components/loading-page.ts
@@ -1,8 +1,57 @@
 import { Vue, Component } from "vue-property-decorator";
-import { applyBeforeQuit, simulateLoading, pageLoaded } from "../helpers/loading";
+import { simulateLoading, pageLoaded } from "../helpers/loading";
 import taggr from "../devtools/taggr";
 
 const log = taggr("loading-page");
+
+/**
+ * Replace all links events to run the callback before loading the new page
+ * @param {() => Promise<any>} callback
+ */
+function applyBeforeQuit(callback: () => Promise<any>) {
+    log.info("A loading click Event is added to the document");
+
+    document.addEventListener("click", async (event: MouseEvent) => {
+        log.info("You click on the document");
+
+        // Test for left click.
+        if (event.button !== 0) {
+            log.error("Not a left click");
+            return;
+        }
+
+        let element = event.target as Element | null;
+
+        // Go up through the DOM tree to search for a link
+        while (element && !element.hasAttribute("href")) {
+            element = element.parentElement;
+        }
+
+        if (element) {
+            const location = element.getAttribute("href");
+
+            // If the link is for e-mail, phone or new window, do nothing
+            if ((location && /^((mailto|tel):|#)/.test(location)) || element.hasAttribute("target")) {
+                log.info("This link is for e-mail, phone or a new window");
+                return;
+            }
+
+            event.preventDefault();
+
+            try {
+                await callback();
+            } catch {
+                log.error(`The callback '${callback.name}' failed`);
+            }
+
+            // Once callback returns a promise, go to the new page
+            if (location) {
+                log.info(`Navigate to the new page '${location}'`);
+                window.location.assign(location);
+            }
+        }
+    });
+}
 
 @Component
 class LoadingPage extends Vue {
@@ -17,7 +66,7 @@ class LoadingPage extends Vue {
     private animateEnter() {
         this.isHidden = true;
         this.isLoading = false;
-        log.success("Hello, world!");
+        log.info("Hello, world!");
     }
 
     private animateQuit() {
--- frontend/src/helpers/loading.ts
+++ frontend/src/helpers/loading.ts
@@ -3,55 +3,6 @@ import taggr from "../devtools/taggr";
 const log = taggr("loading");
 
 /**
- * Replace all links events to run the callback before loading the new page
- * @param {() => Promise<any>} callback
- */
-function applyBeforeQuit(callback: () => Promise<any>) {
-    log.info("A loading click Event is added to the document");
-
-    document.addEventListener("click", async (event: MouseEvent) => {
-        log.info("You click on the document");
-
-        // Test for left click.
-        if (event.button !== 0) {
-            log.error("Not a left click");
-            return;
-        }
-
-        let element = event.target as Element | null;
-
-        // Go up through the DOM tree to search for a link
-        while (element && !element.hasAttribute("href")) {
-            element = element.parentElement;
-        }
-
-        if (element) {
-            const location = element.getAttribute("href");
-
-            // If the link is for e-mail, phone or new window, do nothing
-            if ((location && /^((mailto|tel):|#)/.test(location)) || element.hasAttribute("target")) {
-                log.info("This link is for e-mail, phone or a new window");
-                return;
-            }
-
-            event.preventDefault();
-
-            try {
-                await callback();
-            } catch {
-                log.error(`The callback '${callback.name}' failed`);
-            }
-
-            // Once callback returns a promise, go to the new page
-            if (location) {
-                log.info(`Navigate to the new page '${location}'`);
-                window.location.assign(location);
-            }
-        }
-    });
-}
-
-/**
  * A too short loading effect looks buggy.
  * @param {number?} time Time to wait in ms
  * @returns {Promise<*>}
@@ -85,4 +36,4 @@ function pageLoaded(): Promise<any> {
     });
 }
 
-export { applyBeforeQuit, simulateLoading, pageLoaded };
+export { simulateLoading, pageLoaded };
