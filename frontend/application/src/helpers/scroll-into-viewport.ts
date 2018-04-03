1c061b199f7f557563b78b6194f1c808f7041c0f
Cédric Eberhardt <design@cedeber.fr>
2018-04-03 14:34:59 +0200

    new async scrollIntoViewport

--- frontend/application/src/helpers/scroll-into-viewport.ts
+++ frontend/application/src/helpers/scroll-into-viewport.ts
@@ -1,155 +1,247 @@
-declare global {
-    interface HTMLElement {
-        scrollIntoViewport({}?: { speed?: number, marginTop?: number, callback?: string | Function, scrollable?: Element | Window }): Function | number;
-    }
+let scrollIntoViewportAnimationId = 0;
 
-    interface Window {
-        [callback: string]: any;
+/**
+ * like element.scrollIntoView({block: "top", behavior: "smooth"});
+ * @param {number} [topMargin=0] Top margin decal in pixel
+ * @param {number} [speed=35] Average pixel per frame (~ 60fps)
+ * @param {number[]} [bezier=[0.23, 1, 0.32, 1]]
+ * @returns {function|number} the callback if any
+ */
+export default function scrollIntoViewport(
+    topMargin = 0,
+    speed = 35,
+    bezier = [0.23, 1, 0.32, 1],
+) {
+    /**
+     * @param {HTMLElement} element
+     * @returns {Promise<boolean>} true: scrolled; false: already at destination
+     */
+    return async function(element: HTMLElement): Promise<boolean> {
+        const goTo = getTopPosition(element) - topMargin;
+
+        return scrollTo(goTo);
+    };
+
+    /**
+     * @param {number} goTo
+     * @returns {Promise<boolean>}
+     */
+    async function scrollTo(goTo: number): Promise<any> {
+        return new Promise((resolve, _reject) => {
+            const startTime = Date.now();
+            const scrollOffset = window.pageYOffset; // or pageYOffset=scrollY
+            const pageHeight = Math.max(
+                document.body.scrollHeight,
+                document.documentElement.scrollHeight,
+            );
+            const windowHeight = window.innerHeight;
+
+            /** @type {function(number): number} */
+            const easing = bezierEasing(bezier);
+
+            let delta = goTo - scrollOffset;
+            const duration = Math.abs(delta / (speed * 60) * 1000);
+            let toGo = scrollOffset;
+
+            // limit to not scroll more than the page can scroll
+            // if the target position is low in the page
+            const next = pageHeight - goTo;
+
+            if (next < windowHeight) {
+                delta = delta - (windowHeight - next);
+            }
+
+            window.cancelAnimationFrame(scrollIntoViewportAnimationId);
+
+            if (delta !== 0) {
+                scrollIntoViewportAnimationId = window.requestAnimationFrame(step);
+
+                return;
+            }
+
+            // already at destination
+            return resolve(false);
+
+            /**
+             * Scroll to the element
+             * @returns {*}
+             */
+            function step() {
+                const delay = Date.now() - startTime;
+                const easingVariation = easing(delay / duration);
+
+                toGo = easingVariation > 1
+                    ? goTo // if scroll too much
+                    : Math.ceil(delta * easingVariation + scrollOffset);
+
+                // Still need to scroll
+                if (easingVariation < 1 && toGo !== goTo) {
+                    window.scrollTo(0, toGo); // or scroll()
+                    scrollIntoViewportAnimationId = window.requestAnimationFrame(step);
+
+                    return;
+                }
+
+                // Scroll done
+                return resolve(true);
+            }
+        });
     }
 }
 
-import taggr from "../devtools/taggr";
-
-let scrollIntoViewportAnimationId: number = 0;
-
 /**
  * Get top position of an element in the page
- * @returns the top position in pixels
+ * @param {HTMLElement} element
+ * @param {Element|Window} [boundary=window]
+ * @returns {number} the top position in pixels
  */
-function getTopPosition(element: HTMLElement, boundary: Element | Window = window): number {
+function getTopPosition(element: any, boundary = window) {
     let top = element.offsetTop;
 
-    // tslint:disable-next-line:no-conditional-assignment
-    while ((element = element.offsetParent as HTMLElement) !== null && element !== boundary) {
+    while (
+        (element = /** @type {HTMLElement} */ (element.offsetParent)) !== null &&
+        element !== boundary
+        ) {
         top += element.offsetTop;
     }
 
     return top;
 }
 
-/**
- * Easing functions: easeOutCubic
- * @param time Current time / current frame
- * @param begin Start value
- * @param change Change in value
- * @param duration Duration / number of frames
- * @returns the next value
- */
-function easeOutCubic(time: number, begin: number, change: number, duration: number): number {
-    return change * ((time = time / duration - 1) * time * time + 1) + begin;
-}
-
-/**
- * like element.scrollIntoView({block: "top", behavior: "smooth"});
- * @todo return a Promise
- * @param speed Average pixel per frame (~ 60fps)
- * @param marginTop Top margin decal in pixel
- * @param callback Callback function
- * @param scrollable Element to scroll
- * @returns the callback if any
- */
-HTMLElement.prototype.scrollIntoViewport = function({ speed = 35, marginTop = 0, callback = null, scrollable = window }:
-    { speed?: number, marginTop?: number, callback?: string | Function, scrollable?: HTMLElement | Window } = {}): Function | number {
-    const log = taggr("scroll-into-viewport").keep(this);
-
-    const start = Date.now();
-    const offset = scrollable === window ? window.pageYOffset : (scrollable as HTMLElement).scrollTop; // or pageYOffset=scrollY
-    const goTo =
-        getTopPosition(this, scrollable) -
-        marginTop -
-        (scrollable === window ? 0 : getTopPosition(scrollable as HTMLElement, scrollable));
-    const pageHeight =
-        scrollable === window
-            ? Math.max(document.body.scrollHeight, document.documentElement.scrollHeight)
-            : (scrollable as HTMLElement).scrollHeight;
-    const windowHeight = scrollable === window ? window.innerHeight : (scrollable as HTMLElement).clientHeight;
-    const next = pageHeight - goTo;
-
-    let toGo = offset;
-    let delta = goTo - offset;
-    let change = delta;
-
-    let timeStart = 0;
-
-    // Prevent win declaration to an element without Y scroll overflow
-    if (
-        scrollable !== window &&
-        (scrollable as HTMLElement).clientHeight === (scrollable as HTMLElement).scrollHeight
-    ) {
-        scrollable = window;
-    }
-
-    window.cancelAnimationFrame(scrollIntoViewportAnimationId);
-
-    if (next < windowHeight) {
-        delta = delta - (windowHeight - next);
-        change = delta;
-    }
-
+function bezierEasing(bezierArray: number[]) {
     /**
-     * Scroll to the element
-     * @returns the callback if any
+     * https://github.com/gre/bezier-easing
+     * BezierEasing - use bezier curve for transition easing function
+     * by Gaëtan Renaudeau 2014 - 2015 – MIT License
      */
-    function step(): Function | number {
-        const whereAmI = scrollable === window ? window.pageYOffset : (scrollable as HTMLElement).scrollTop;
 
-        if (toGo < whereAmI - 1 || toGo > whereAmI + 1) {
-            log.error("Scroll animation cancelled");
+    // These values are established by empiricism with tests (tradeoff: performance VS precision)
+    const NEWTON_ITERATIONS = 4;
+    const NEWTON_MIN_SLOPE = 0.001;
+    const SUBDIVISION_PRECISION = 0.0000001;
+    const SUBDIVISION_MAX_ITERATIONS = 10;
 
-            // for high definition screens, some browsers doesn't return a integer if
-            // the screen's definition is not an integer, unlike Retina displays.
-            try {
-                scrollable.dispatchEvent(new Event("scrollcancel"));
-            } catch {
-                /* empty */
-            }
+    const kSplineTableSize = 11;
+    const kSampleStepSize = 1.0 / (kSplineTableSize - 1.0);
 
-            return 1;
-        }
+    const float32ArraySupported = typeof Float32Array === "function";
 
-        change = speed > 0 ? change - easeOutCubic(Date.now() - start, 0, change, Math.abs(delta) / speed * 1000) : 0;
-        toGo = Math.floor(offset + delta - change + 1);
+    function A(aA1: number, aA2: number): number {
+        return 1.0 - 3.0 * aA2 + 3.0 * aA1;
+    }
 
-        if (Math.abs(Math.abs(delta) - Math.abs(change)) < Math.abs(delta) - 1) {
-            if (scrollable === window) {
-                window.scrollTo(0, toGo); // or scroll()
+    function B(aA1: number, aA2: number): number {
+        return 3.0 * aA2 - 6.0 * aA1;
+    }
+
+    function C(aA1: number): number {
+        return 3.0 * aA1;
+    }
+
+    // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
+    function calcBezier(aT: number, aA1: number, aA2: number): number {
+        return ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT;
+    }
+
+    // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
+    function getSlope(aT: number, aA1: number, aA2: number): number {
+        return 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1);
+    }
+
+    function binarySubdivide(aX: number, aA: number, aB: number, mX1: number, mX2: number): number {
+        let currentX;
+        let currentT;
+        let i = 0;
+        do {
+            currentT = aA + (aB - aA) / 2.0;
+            currentX = calcBezier(currentT, mX1, mX2) - aX;
+            if (currentX > 0.0) {
+                aB = currentT;
             } else {
-                (scrollable as HTMLElement).scrollTop = toGo;
+                aA = currentT;
             }
-
-            scrollIntoViewportAnimationId = window.requestAnimationFrame(step);
-        } else {
-            log.success(`Scroll animation done is ${(performance.now() - timeStart).toFixed()}ms`);
-
-            if (typeof callback === "function") {
-                return callback();
-            }
-
-            if (typeof callback === "string" && typeof window[callback] === "function") {
-                return window[callback]();
-            }
-        }
-
-        return 1;
+        } while (
+            Math.abs(currentX) > SUBDIVISION_PRECISION &&
+            ++i < SUBDIVISION_MAX_ITERATIONS
+            );
+        return currentT;
     }
 
-    if (change === 0) {
-        log.success("Scroll animation already on position");
-
-        if (typeof callback === "function") {
-            return callback();
+    function newtonRaphsonIterate(aX: number, aGuessT: number, mX1: number, mX2: number): number {
+        for (let i = 0; i < NEWTON_ITERATIONS; ++i) {
+            const currentSlope = getSlope(aGuessT, mX1, mX2);
+            if (currentSlope === 0.0) {
+                return aGuessT;
+            }
+            const currentX = calcBezier(aGuessT, mX1, mX2) - aX;
+            aGuessT -= currentX / currentSlope;
         }
-
-        if (typeof callback === "string" && typeof window[callback] === "function") {
-            return window[callback]();
-        }
-    } else {
-        log.info(
-            `Will scroll from position ${offset}px to ${goTo}px (${delta}px) with a top margin of ${marginTop}px and at ${speed}px per frame`,
-        );
-        timeStart = performance.now();
-        scrollIntoViewportAnimationId = window.requestAnimationFrame(step);
+        return aGuessT;
     }
 
-    return 0;
-};
+    return (function bezier([mX1, mY1, mX2, mY2] = bezierArray) {
+        if (!(0 <= mX1 && mX1 <= 1 && 0 <= mX2 && mX2 <= 1)) {
+            throw new Error("bezier x values must be in [0, 1] range");
+        }
+
+        // Precompute samples table
+        const sampleValues = float32ArraySupported
+            ? new Float32Array(kSplineTableSize)
+            : new Array(kSplineTableSize);
+        if (mX1 !== mY1 || mX2 !== mY2) {
+            for (let i = 0; i < kSplineTableSize; ++i) {
+                sampleValues[i] = calcBezier(i * kSampleStepSize, mX1, mX2);
+            }
+        }
+
+        function getTForX(aX: number): number {
+            let intervalStart = 0.0;
+            let currentSample = 1;
+            const lastSample = kSplineTableSize - 1;
+
+            for (
+                ;
+                currentSample !== lastSample && sampleValues[currentSample] <= aX;
+                ++currentSample
+            ) {
+                intervalStart += kSampleStepSize;
+            }
+            --currentSample;
+
+            // Interpolate to provide an initial guess for t
+            const dist =
+                (aX - sampleValues[currentSample]) /
+                (sampleValues[currentSample + 1] - sampleValues[currentSample]);
+            const guessForT = intervalStart + dist * kSampleStepSize;
+
+            const initialSlope = getSlope(guessForT, mX1, mX2);
+            if (initialSlope >= NEWTON_MIN_SLOPE) {
+                return newtonRaphsonIterate(aX, guessForT, mX1, mX2);
+            } else if (initialSlope === 0.0) {
+                return guessForT;
+            } else {
+                return binarySubdivide(
+                    aX,
+                    intervalStart,
+                    intervalStart + kSampleStepSize,
+                    mX1,
+                    mX2,
+                );
+            }
+        }
+
+        return function BezierEasing(x: number): number {
+            if (mX1 === mY1 && mX2 === mY2) {
+                return x; // linear
+            }
+            // Because JavaScript number are imprecise, we should guarantee the extremes are right.
+            if (x === 0) {
+                return 0;
+            }
+            if (x === 1) {
+                return 1;
+            }
+            return calcBezier(getTForX(x), mY1, mY2);
+        };
+    })();
+}
