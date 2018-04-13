5ebaa8e7ec8753b16bbb7d75e479077b446ec31b
Cédric Eberhardt <design@cedeber.fr>
2018-04-13 09:41:12 +0200

    taggr v1.3

--- frontend/application/src/devtools/taggr.ts
+++ frontend/application/src/devtools/taggr.ts
@@ -1,56 +1,7 @@
+/*! DevTools Taggr v1.3 */
 // tslint:disable:no-console
 
 declare var process: any;
-
-/**
- * Stylings for the log
- * @param {number} lvl Error level
- * @returns {string} Developer tools console stylings
- */
-function getStyle(lvl: number) {
-    const bgColor = ["#2d2d2d", "#6699cc", "#99cc99", "#ffcc66", "#f2777a"];
-    const txtColor = ["#ffcc66", "#f2f0ec", "#2d2d2d", "#2d2d2d", "#f2f0ec"];
-
-    return lvl > 0
-        ? `background: ${bgColor[lvl] || bgColor[0]}; color: ${txtColor[lvl] ||
-              txtColor[0]}; padding: 2px 0.9em 2px 0.7em; border-top-right-radius: 1.5em; border-bottom-right-radius: 1.5em; font-weight: normal; margin-right: 0.5em;`
-        : `background: ${bgColor[lvl] || bgColor[0]}; color: ${txtColor[lvl] ||
-              txtColor[0]}; padding: 2px 0.7em 2px 0.9em; border-top-left-radius: 1.5em; border-bottom-left-radius: 1.5em; font-weight: bold; margin-right: 1px;`;
-}
-
-/**
- * Print the messages to the console
- * @param {number} level Error level
- * @param {string | undefined} tag
- * @param {string | undefined} message
- * @param args
- */
-function spray(level: number, tag: string | undefined, message: string | undefined, ...args: any[]) {
-    // Only print message in development mode
-    if (process.env.NODE_ENV === "development") {
-        const defaultLogArgs = [
-            `%ctaggr%c${tag || "»"}%c${message || ""}`,
-            getStyle(0),
-            getStyle(level),
-            "color: #2d2d2d; font-weight: bold",
-        ];
-
-        if (args.length > 0) {
-            // Create a console group if you list stuff to output
-            console.groupCollapsed(...defaultLogArgs);
-
-            for (const arg of args) {
-                console.log(arg);
-            }
-
-            // Close the group
-            console.groupEnd();
-        } else {
-            // or simply print the message of nothing need to be listed
-            console.log(...defaultLogArgs);
-        }
-    }
-}
 
 class Tagger {
     private tag: string | undefined;
@@ -70,8 +21,10 @@ class Tagger {
      * @param {string} message
      * @returns {Tagger} This Tagger instance
      */
-    private print(level: number, message?: string) {
+    private print(level: number, message?: string): Tagger {
         spray(level, this.tag, message, ...[...this.keptArgs, ...this.args]);
+
+        // Clean the list
         this.args = [];
 
         return this;
@@ -82,7 +35,7 @@ class Tagger {
      * @param {string} message
      * @returns {Tagger} This Tagger instance
      */
-    public info(message?: string) {
+    public info(message?: string): Tagger {
         return this.print(1, message);
     }
 
@@ -91,7 +44,7 @@ class Tagger {
      * @param {string} message
      * @returns {Tagger} This Tagger instance
      */
-    public success(message?: string) {
+    public success(message?: string): Tagger {
         return this.print(2, message);
     }
 
@@ -100,7 +53,7 @@ class Tagger {
      * @param {string} message
      * @returns {Tagger} This Tagger instance
      */
-    public warning(message?: string) {
+    public warning(message?: string): Tagger {
         return this.print(3, message);
     }
 
@@ -109,7 +62,7 @@ class Tagger {
      * @param {string} message
      * @returns {Tagger} This Tagger instance
      */
-    public error(message?: string) {
+    public error(message?: string): Tagger {
         return this.print(4, message);
     }
 
@@ -117,13 +70,12 @@ class Tagger {
      * Add stuff to output into a console group
      * To output the group you need to send a message after having listed some other information
      * The list is cleaned each time you send a message
-     * @example
-     * log.list([SOMETHING]).list([SOMETHING ELSE]).info("[A MESSAGE]")
      * @param args
      * @returns {Tagger} This Tagger instance
      */
-    public list(...args: any[]) {
+    public list(...args: any[]): Tagger {
         this.args.push(...args);
+
         return this;
     }
 
@@ -131,13 +83,10 @@ class Tagger {
      * Add stuff to output into a console group
      * To output the group you need to send a message after having listed some other information
      * The kept arguments won't be deleted after you send a message
-     * @example
-     * log.keep([SOMETHING]).list([SOMETHING ELSE]).info("[A MESSAGE]")
-     * log.info("[ANOTHER MESSAGE]") // => [SOMETHING] will be outputed into a group
      * @param args
      * @returns {Tagger} This Tagger instance
      */
-    public keep(...args: any[]) {
+    public keep(...args: any[]): Tagger {
         this.keptArgs.push(...args);
         return this;
     }
@@ -145,13 +94,74 @@ class Tagger {
 
 /**
  * Create a new tagged Tagger
- * @example
- * const log = taggr("[A TAG]");
- * @param {string} tag
+ * @param {string} [tag]
  * @returns {Tagger} A new Tagger instance
  */
-function taggr(tag?: string) {
+export default function taggr(tag?: string): Tagger {
     return new Tagger(tag);
 }
 
-export default taggr;
+/**
+ * Print the messages to the console
+ * @param {number} level Error level
+ * @param {string | undefined} tag
+ * @param {string | undefined} message
+ * @param args
+ */
+function spray(
+    level: number,
+    tag: string | undefined,
+    message: string | undefined,
+    ...args: any[]
+) {
+    // Only print message in development mode
+    if (process.env.NODE_ENV === "development") {
+        const defaultLogArgs = [
+            `%ctaggr%c${tag || "»"}%c${message || ""}`,
+            getStyle(0),
+            getStyle(level),
+            "color: #2d2d2d; font-weight: bold",
+        ];
+
+        if (args && args.length > 0) {
+            // Create a console group if you list stuff to output
+            console.groupCollapsed(...defaultLogArgs);
+
+            for (const arg of args) {
+                console.log(arg);
+            }
+
+            // Close the group
+            console.groupEnd();
+        } else {
+            // or simply print the message of nothing need to be listed
+            console.log(...defaultLogArgs);
+        }
+    }
+}
+
+/**
+ * Stylings for the log
+ * @param {number} lvl Error level
+ * @returns {string} Developer tools console stylings
+ */
+function getStyle(lvl: number) {
+    const bgColor = ["#2d2d2d", "#6699cc", "#99cc99", "#ffcc66", "#f2777a"];
+    const txtColor = ["#ffcc66", "#f2f0ec", "#2d2d2d", "#2d2d2d", "#f2f0ec"];
+
+    return lvl > 0
+        ? `background: ${bgColor[lvl] || bgColor[0]};` +
+              `color: ${txtColor[lvl] || txtColor[0]};` +
+              "padding: 2px 0.9em 2px 0.7em;" +
+              "border-top-right-radius: 1.5em;" +
+              "border-bottom-right-radius: 1.5em;" +
+              "font-weight: normal;" +
+              "margin-right: 0.5em;"
+        : `background: ${bgColor[lvl] || bgColor[0]};` +
+              `color: ${txtColor[lvl] || txtColor[0]};` +
+              `padding: 2px 0.7em 2px 0.9em;` +
+              `border-top-left-radius: 1.5em;` +
+              `border-bottom-left-radius: 1.5em;` +
+              `font-weight: bold;` +
+              `margin-right: 1px;`;
+}
