e8bd071d6cd13d5d401972dd2b67f42f0ec15861
Cédric Eberhardt <design@cedeber.fr>
2018-06-15 12:00:01 +0200

    add append polyfill

--- frontend/application/polyfills.js
+++ frontend/application/polyfills.js
@@ -32,3 +32,35 @@ try {
     MouseEvent.prototype = Event.prototype;
     window.MouseEvent = MouseEvent;
 }
+
+/**
+ * append() polyfill
+ * @see https://github.com/jserz/js_piece/blob/master/DOM/ParentNode/append()/append().md
+ */
+(function(arr) {
+    arr.forEach(function(item) {
+        if (item.hasOwnProperty("append")) {
+            return;
+        }
+        Object.defineProperty(item, "append", {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: function append() {
+                var argArr = Array.prototype.slice.call(arguments),
+                    docFrag = document.createDocumentFragment();
+
+                argArr.forEach(function(argItem) {
+                    var isNode = argItem instanceof Node;
+                    docFrag.appendChild(
+                        isNode
+                            ? argItem
+                            : document.createTextNode(String(argItem)),
+                    );
+                });
+
+                this.appendChild(docFrag);
+            },
+        });
+    });
+})([Element.prototype, Document.prototype, DocumentFragment.prototype]);
