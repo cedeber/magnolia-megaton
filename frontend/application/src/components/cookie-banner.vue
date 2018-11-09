9c721c4cecc2b0b08670f01b66a1f2f1f71ebe69
zischler <tobias.zischler@gmx.ch>
2018-11-09 17:53:00 +0100

    - check if hostname is null to build the cookie banner

--- frontend/application/src/components/cookie-banner.vue
+++ frontend/application/src/components/cookie-banner.vue
@@ -110,9 +110,11 @@
 
         deleteCookie(cookieName) {
             let hostname = window.document.location.hostname;
-            0 === hostname.indexOf("www.") && (hostname = hostname.substring(4)); // TODO: WHAT IS THIS??!
-            window.document.cookie = cookieName + "=; domain=." + hostname + "; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/";
-            window.document.cookie = cookieName + "=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/"
+            if(hostname != null){
+                0 === hostname.indexOf("www.") && (hostname = hostname.substring(4)); // TODO: WHAT IS THIS??!
+                window.document.cookie = cookieName + "=; domain=." + hostname + "; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/";
+                window.document.cookie = cookieName + "=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/"
+            }
         }
     }
 </script>
